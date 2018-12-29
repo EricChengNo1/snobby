b39cabb511d369b9c781e90521abba9a5bd9d676
eric_cheng <961719622@qq.com>
2018-12-29 17:18:11 +0800

    静态资源

---
+++ precache-manifest.66e6ac4dbe00e1ce490c557fdeed14e2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d8d9b1ad96a6e9cad510db232779cf7",
+    "url": "./static/media/cust1.3d8d9b1a.png"
+  },
+  {
+    "revision": "0a300f29cf2ac9b54e80",
+    "url": "./static/css/main.57a18332.chunk.css"
+  },
+  {
+    "revision": "4f0a7ea3ad378303e8dbaaa9cd92c06b",
+    "url": "./static/media/banner.4f0a7ea3.png"
+  },
+  {
+    "revision": "716b38813f9d7b9625f6",
+    "url": "./static/js/1.716b3881.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ab7ad079b4e07b14f08b5ae7bf237dd6",
+    "url": "./static/media/user-image.ab7ad079.png"
+  },
+  {
+    "revision": "0a300f29cf2ac9b54e80",
+    "url": "./static/js/main.0a300f29.chunk.js"
+  },
+  {
+    "revision": "fdac8e29923182d9c033f63433b049bd",
+    "url": "./static/media/cust2.fdac8e29.png"
+  },
+  {
+    "revision": "592ea735e0cf0451c92599bfc0668415",
+    "url": "./static/media/cust3.592ea735.png"
+  },
+  {
+    "revision": "188d5a9864169cc266fb64f0cefa5ca2",
+    "url": "./static/media/cust4.188d5a98.png"
+  },
+  {
+    "revision": "716b38813f9d7b9625f6",
+    "url": "./static/css/1.12b91e05.chunk.css"
+  },
+  {
+    "revision": "4e0071a9691cf066c979544730f6e7c9",
+    "url": "./index.html"
+  }
+];
